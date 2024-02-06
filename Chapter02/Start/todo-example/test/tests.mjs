4d7c0c0a5e791c62e0c8054703117f0b3b3c112e
Augusto Görgen <dev.gorgen@gmail.com>
2024-02-06 18:22:23 +0000

    Finishing chapter 3

--- Chapter02/Start/todo-example/test/tests.mjs
+++ Chapter02/Start/todo-example/test/tests.mjs
@@ -1,50 +1,67 @@
 import { emptyTodo, markAsDone } from "../src/todo.mjs";
 import { TodoRepository } from "../src/todoRepository.mjs";
+import { it } from "concise-test";
 
-const todo = emptyTodo();
+it("sets completeAt when calling markAsDone", () => {
+  const todo = emptyTodo();
 
-if (!markAsDone(todo).completedAt)
-  throw new Error(
-    "completedAt not set when calling markAsDone"
-  );
-
-const repository = new TodoRepository();
-
-try {
-  repository.add(emptyTodo());
-  throw new Error(
-    "no error thrown when adding an empty todo"
-  );
-} catch (e) {
-  if (e.message !== "title cannot be blank")
+  if (!markAsDone(todo).completedAt)
     throw new Error(
-      "wrong message in guard clause when adding an empty todo"
-    );
-}
+      "completedAt not set when calling markAsDone"
+    );  
+});
 
-const newTodo = { ...emptyTodo(), title: "test" };
-repository.add(newTodo);
 
-const repeatedTodo = { ...newTodo };
-try {
-  repository.add(repeatedTodo);
-  throw new Error(
-    "no error thrown when adding a repeated todo"
-  );
-} catch (e) {
-  if (e.message !== "todo already exists")
+it("throws an exception when adding a todo without a title", () => {
+  try {
+    const repository = new TodoRepository();
+    repository.add(emptyTodo());
     throw new Error(
-      "wrong message in guard clause when adding an existing todo"
+      "no error thrown when adding an empty todo"
     );
-}
+  } catch (e) {
+    if (e.message !== "title cannot be blank")
+      throw new Error(
+        "wrong message in guard clause when adding an empty todo"
+      );
+  }
+});
 
-if (repository.findAllMatching("").length !== 1)
-  throw new Error("added todo was not returned");
+it("throws errors when adding a repeated todo", () => {
+  const repository = new TodoRepository();
+  const newTodo = { ...emptyTodo(), title: "test" };
+  repository.add(newTodo);
+  
+  const repeatedTodo = { ...newTodo };
+  try {
+    repository.add(repeatedTodo);
+    throw new Error(
+      "no error thrown when adding a repeated todo"
+    );
+  } catch (e) {
+    if (e.message !== "todo already exists")
+      throw new Error(
+        "wrong message in guard clause when adding an existing todo"
+      );
+  }
+});
 
-if (
-  repository.findAllMatching("some other test").length !==
-  0
-)
-  throw new Error(
-    "filter was not applied when finding matches"
-  );
+it("finds an added todo", () => {
+    const repository = new TodoRepository();
+    const newTodo = { ...emptyTodo(), title: "test" };
+    repository.add(newTodo);
+    if (repository.findAllMatching("").length !== 1)
+      throw new Error("added todo was not returned");
+});
+
+it("filters out todos that do not match filter", () => {
+  const repository = new TodoRepository();
+
+  if (    
+    repository.findAllMatching("some other test").length !==
+    0
+  )
+    throw new Error(
+      "filter was not applied when finding matches"
+    );
+})
